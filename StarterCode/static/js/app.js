680ec643768f935b14f0fca66d93e109a4ba13ca
Jayden LeBlanc <jaydenleblanc@gmail.com>
2020-09-27 01:35:28 -0400

    Ids Array

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,17 @@
-//Bring in JSON file and console log it
+// Bring in JSON file and console log it
 d3.json("samples.json").then(function (data) {
-  console.log(data[0]);
+  //console.log(data);
+
+  // Pull out samples array
+  var samples = data[0].samples;
+  console.log(samples);
+  //console.log(samples[0].id);
+
+  // Array to hold sample ID's
+  var dropdownId = [];
+
+  for (var i = 0; i < samples.length; i++) {
+    dropdownId.push(samples[i].id);
+  }
+  console.log(dropdownId);
 });
